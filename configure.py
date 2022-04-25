9b67df37cffb35dbc7008a7201a628c1330c8780
Abhijeet Pal <38559396+TheAbhijeet@users.noreply.github.com>
2022-04-25 12:27:15 +0530

    Support for python3.9+ (#5)
    Update mapping imports

--- configure.py
+++ configure.py
@@ -5,7 +5,10 @@
 
 """
 import sys
-from collections import Mapping, MutableMapping
+try:
+    from collections.abc import Mapping, MutableMapping
+except ImportError:
+    from collections import Mapping, MutableMapping
 from datetime import timedelta
 from inspect import getargspec
 
